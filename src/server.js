10231262f6d252dcff4c8128db2eec32eda6638b
nqba2509 <nqba2509@gmail.com>
2024-08-14 00:31:12 +0700

    connectDB and test View data

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import viewEngine from './config/viewEngine'
 import initWebRouter from './route/web'
+import connectDB from "./config/connectDB"
 require('dotenv').config()
 
 let app = express()
@@ -14,6 +15,8 @@ app.use(bodyParser.urlencoded(
 
 viewEngine(app);
 initWebRouter(app)
+
+connectDB()
 
 let port = process.env.PORT || 8888;
 app.listen(port, () => {
